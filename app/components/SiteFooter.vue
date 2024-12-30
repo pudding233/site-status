caae9b4995aa9971e20ff64d5bc3cf5a88623e07
pudding233 <33753572+pudding233@users.noreply.github.com>
2024-12-31 02:38:21 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/pudding233",
+  home: "https://www.furryowo.top",
+  email: "mailto:im@furryowo.top",
 };
 </script>
 
